f8bc19ec0a80cdb65757922f98c573735d1385e9
johnnyig <johnny.ghabbour@gmail.com>
2020-05-18 23:08:16 -0500

    Update Array_math_cunit.c

--- Array_math_cunit.c
+++ Array_math_cunit.c
@@ -1,7 +1,11 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int arr_elemnts;
+int* tst_arr1;
+int* tst_arr2;
 
 
 int get_array_max (int* a, int b){
@@ -67,24 +71,29 @@ void test_array_math(void){
 	CU_ASSERT(get_array_max(tst_arr1,arr_elemnts) == 10);
 	CU_ASSERT(get_array_min(tst_arr1,arr_elemnts) == -5);
 	CU_ASSERT(get_array_avg(tst_arr1,arr_elemnts) == 4.2);
-	CU_ASSERT(get__array_sum(tst_arr1,arr_elemnts) == 21);
+	CU_ASSERT(get_array_sum(tst_arr1,arr_elemnts) == 21);
 	CU_ASSERT(array_pstvs(tst_arr1,arr_elemnts) == 4);
 	CU_ASSERT(array_ngtvs(tst_arr1,arr_elemnts) == 1);
 	
 	CU_ASSERT(get_array_max(tst_arr2,arr_elemnts) == 1);
 	CU_ASSERT(get_array_min(tst_arr2,arr_elemnts) == -1);
 	CU_ASSERT(get_array_avg(tst_arr2,arr_elemnts) == 0.0);
-	CU_ASSERT(get__array_sum(tst_arr2,arr_elemnts) == 0);
+	CU_ASSERT(get_array_sum(tst_arr2,arr_elemnts) == 0);
 	CU_ASSERT(array_pstvs(tst_arr2,arr_elemnts) == 1);
 	CU_ASSERT(array_ngtvs(tst_arr2,arr_elemnts) == 1);
 	}
 
 int main() {
-	int arr_elemnts = 5;
-	int tst_arr1[arr_elemnts] = {1,8,10,7,-5};
-	int tst_arr2[arr_elemnts] = {1,0,0,0,-1};
-
-	
+	int i;
+	arr_elemnts = 5;
+	int arr1[5] = {1,8,10,7,-5};
+	int arr2[5] = {1,0,0,0,-1};
+	tst_arr1 = malloc(5*sizeof(int));
+	tst_arr2 = malloc(5*sizeof(int));
+	for (i=0; i<arr_elemnts; i++){
+		tst_arr1[i] = arr1[i];
+		tst_arr2[i] = arr2[i];
+	}
     CU_initialize_registry();
     CU_pSuite suite = CU_add_suite("Array_math_test",0,0);
 	CU_add_test(suite, "array_math", test_array_math);
