e07ccbf9288759065f50f421b731571c1256c950
johnnyig <johnny.ghabbour@gmail.com>
2020-05-18 23:10:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -7,8 +7,6 @@ int main()
 	int arr[10] = {2, 3, 4, 5, 8, 12, 102, 512, 0, -1};
 
 	f=get_array_max (arr, 10);
-	printf("Add:%d\n",c);
-	printf("Subtract:%d\n",d);
 	printf("Array largest value:%d\n",f);
 	
 	printf("Original array\n");
